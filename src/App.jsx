bec1c4166a6ca95d412d7b339ef301f7418994b9
parkseyoung <hellosy76@naver.com>
2024-09-27 15:41:38 +0900

    feat: routes 주소 추가 (정보변경 라우트 생성)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import StartRoutes from "./routes/StartRoutes";
+import SettingRoutes from "./routes/SettingRoutes";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <StartRoutes />
+      <SettingRoutes />
     </Router>
   );
 }
