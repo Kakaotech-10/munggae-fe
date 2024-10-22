54512e164deb998b59df50f11cba1865b4965a26
hardlife0 <102363884+hardlife0@users.noreply.github.com>
2024-10-22 10:30:03 +0900

    Revert "✨커뮤니티 페이지 일부 동작 구현"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import StartRoutes from "./routes/StartRoutes";
-import SettingRoutes from "./routes/SettingRoutes";
 import "./App.css";
-
+import MainForm from "./containers/MainForm";
 function App() {
   return (
-    <Router>
-      <StartRoutes />
-      <SettingRoutes />
-    </Router>
+    <>
+      <MainForm />
+    </>
   );
 }
 
--- src/constant/storageKey.js
+++
@@ -1,3 +0,0 @@
-export const ACCESS_TOKEN_KEY = "accesstoken";
-export const REFRESH_TOKEN_KEY = "refreshtoken";
-export const USER_ID_KEY = "member_id";
