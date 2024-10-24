8456c5edcc54dde6fa01d0dfe90b11d80f8b1ddb
parkseyoung <hellosy76@naver.com>
2024-10-24 15:18:35 +0900

    feat: setting UX/UI 구현하기

--- src/containers/ChangePass.jsx
+++ src/containers/ChangePass.jsx
@@ -1,7 +1,9 @@
 import Sidebar from "./SideForm";
 import Button from "../component/Button";
 import Input from "../component/Input";
+import Search from "../component/Search";
 import "./styles/ChangePass.scss";
+import Logo from "../image/logo_black.png";
 
 const ChangePass = () => {
   return (
@@ -10,7 +12,13 @@ const ChangePass = () => {
         <Sidebar showLogout={false} />
       </div>
       <div className="content-wrapper">
+        <div className="search-area">
+          <Search />
+        </div>
+
         <div className="password-area">
+          <h3>비밀번호 변경</h3>
+          <img className="settinglogo" src={Logo} />
           <Input type="password" placeholder="비밀번호" />
           <Input type="password" placeholder="비밀번호 확인" />
           <Button text="비밀번호 변경하기" />
