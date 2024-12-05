cf4fe0999e8bab8b2fe619b5afadd6d6c64eb577
parkseyoung <hellosy76@naver.com>
2024-12-05 17:40:51 +0900

    feat: 게시물 클릭시 보이는 페이지 레이아웃 제작

--- src/routes/StudyRoutes.jsx
+++ src/routes/StudyRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import StudyWriteForm from "../containers/StudyWriteForm";
 import StudyForm from "../containers/StudyForm";
 import ProtectedRoute from "./ProtectedRoute";
+import StudyViewForm from "../containers/StudyViewForm";
 
 function StudyRoutes() {
   return (
@@ -19,6 +20,14 @@ function StudyRoutes() {
         element={
           <ProtectedRoute>
             <StudyWriteForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="studyviewpage"
+        element={
+          <ProtectedRoute>
+            <StudyViewForm />
           </ProtectedRoute>
         }
       />
