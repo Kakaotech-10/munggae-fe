4a0fbb7736b26a2d4e4ce08cba7f67142148fa6a
parkseyoung <hellosy76@naver.com>
2024-12-23 19:35:11 +0900

    bug: 필터링

--- src/component/FilteredContent.jsx
+++ src/component/FilteredContent.jsx
@@ -1,33 +1,16 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-import "./styles/FilteredContent.scss";
 
-const FilteredContent = ({ title, content, codeArea, clean = true }) => {
+const FilteredContent = ({ title, content, codeArea, clean }) => {
   const [isRevealed, setIsRevealed] = useState(false);
 
-  if (clean) {
+  // Explicitly check if clean is false
+  const shouldFilter = clean === false;
+
+  // If content should be filtered but not yet revealed
+  if (shouldFilter && !isRevealed) {
     return (
       <div className="filtered-content">
-        <h4 className="title">{title}</h4>
-
-        {/* 일반 텍스트 영역 */}
-        <div className="content-text">{content}</div>
-
-        {/* 코드 영역이 있는 경우에만 표시 */}
-        {codeArea && codeArea.trim() !== "" && (
-          <div className="code-area">
-            <pre>
-              <code>{codeArea}</code>
-            </pre>
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  return (
-    <div className="filtered-content">
-      {!isRevealed ? (
         <div className="filtered-message">
           <span className="warning-text">
             [구름봇에 의해 가려진 게시물입니다]
@@ -36,21 +19,20 @@ const FilteredContent = ({ title, content, codeArea, clean = true }) => {
             확인하기
           </button>
         </div>
-      ) : (
-        <div className="revealed-content">
-          <h4 className="title">{title}</h4>
+      </div>
+    );
+  }
 
-          {/* 일반 텍스트 영역 */}
-          <div className="content-text">{content}</div>
-
-          {/* 코드 영역이 있는 경우에만 표시 */}
-          {codeArea && codeArea.trim() !== "" && (
-            <div className="code-area">
-              <pre>
-                <code>{codeArea}</code>
-              </pre>
-            </div>
-          )}
+  // Show content if either clean=true or content has been revealed
+  return (
+    <div className="filtered-content">
+      <h4 className="title">{title}</h4>
+      <div className="content-text">{content}</div>
+      {codeArea && codeArea.trim() !== "" && (
+        <div className="code-area">
+          <pre>
+            <code>{codeArea}</code>
+          </pre>
         </div>
       )}
     </div>
